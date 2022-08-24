de37da424e3f6cb95db9a082ae305672d6c034c3
KaganDogan <kagandoganx@gmail.com>
2022-08-24 20:17:39 +0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA4zzWWsS-qaAZUQYqOefU7qX0Xbon_dgM",
+  authDomain: "myliba-last-test.firebaseapp.com",
+  projectId: "myliba-last-test",
+  storageBucket: "myliba-last-test.appspot.com",
+  messagingSenderId: "302132485150",
+  appId: "1:302132485150:web:921cb30e5bd024367f220f",
+  measurementId: "G-HZKV5RHKEE"
+  },
 };
